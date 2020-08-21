424e40a7b45a622f5d328d0979485008ba04a4ce
Michael Ford <mtford@gmail.com>
2020-08-21 19:16:09 +0100

    Filter logs

--- src/ui/Root.tsx
+++ src/ui/Root.tsx
@@ -1,11 +1,47 @@
 import * as React from "react";
 
+import styled from "styled-components";
+import { useState } from "react";
 import TabStdIn from "./tabs/TabStdIn";
+import useDebounce from "./use-debounce";
+
+const InputPane = styled.input`
+  width: 100%;
+  padding: 2rem;
+  border-radius: 0;
+  border-left: none;
+  border-right: none;
+  border-bottom: none;
+
+  &:focus {
+    outline: 0;
+  }
+`;
+
+const RootContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ContentPane = styled.div`
+  flex: 1;
+  overflow-y: scroll;
+  overflow-x: hidden;
+`;
 
 export default function Root() {
+  const [filter, setFilter] = useState("");
+
+  const debouncedFilter = useDebounce(filter);
+
   return (
-    <div style={{ width: "100vw" }}>
-      <TabStdIn />
-    </div>
+    <RootContainer>
+      <ContentPane>
+        <TabStdIn filter={debouncedFilter} />
+      </ContentPane>
+      <InputPane value={filter} onChange={(e) => setFilter(e.target.value)} />
+    </RootContainer>
   );
 }
--- src/ui/tabs/TabStdIn.tsx
+++ src/ui/tabs/TabStdIn.tsx
@@ -1,19 +1,10 @@
 import * as React from "react";
-import styled from "styled-components";
 import { useReceiveStdin } from "../use-receive-stdin";
 import { useParseLogNodes } from "../lib/parse/use-parse-log-nodes";
 import { LogRow } from "../components/LogRow";
 
-const Input = styled.input`
-  width: 100%;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  height: 10rem;
-`;
-
-export default function TabStdIn() {
-  const received = useReceiveStdin();
+export default function TabStdIn({ filter }: { filter: string }) {
+  const received = useReceiveStdin({ filter });
 
   const { rows } = useParseLogNodes(received);
 
@@ -22,7 +13,6 @@ export default function TabStdIn() {
       {rows.map((row) => (
         <LogRow key={row.rowid} row={row} />
       ))}
-      <Input />
     </div>
   );
 }
