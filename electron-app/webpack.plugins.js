aa3a012f485ba5cfe583541d8e49e444585441a7
Richard Gill <richard@rgill.co.uk>
2020-10-11 12:35:59 +0100

    attempt to fix lint staged

--- electron-app/webpack.plugins.js
+++ electron-app/webpack.plugins.js
@@ -1,6 +1,6 @@
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
-   module.exports = [
+  module.exports = [
   new ForkTsCheckerWebpackPlugin({
     // async: false,
   }),
